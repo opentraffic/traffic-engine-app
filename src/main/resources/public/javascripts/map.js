daa172641bdabb35874e116a942c6df6e3b9dc54
Xudong Liu <xudong.camsys@gmail.com>
2015-12-24 11:29:40 -0500

    Manage tile layer display order to make sure overlay layers (segmentOverlay etc) are always top above basemap tiles

--- src/main/resources/public/javascripts/map.js
+++ src/main/resources/public/javascripts/map.js
@@ -40,21 +40,22 @@ var Traffic = Traffic || {};
 
 		ENGLISH_TILE_LAYER: L.tileLayer('https://a.tiles.mapbox.com/v4/xdliu.ec1c6353/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieGRsaXUiLCJhIjoiY2lpamVzdGxwMDExN3ZqbTVuOHMwaGVyYyJ9.P_agsd7B_e22deT1QRhiBQ', {
 					attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-					maxZoom: 17
+					maxZoom: 17,
+					zIndex: 0
 				}),
 		LOCALIZED_TILE_LAYER: L.tileLayer('https://a.tiles.mapbox.com/v4/conveyal.gepida3i/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY29udmV5YWwiLCJhIjoiMDliQURXOCJ9.9JWPsqJY7dGIdX777An7Pw', {
 					attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-					maxZoom: 17
+					maxZoom: 17,
+					zIndex: 0
 				}),
 
 		init:function(mapId, options) {
 			options = options || {};
-      options.layers = [this.ENGLISH_TILE_LAYER, this.LOCALIZED_TILE_LAYER];
 
       this.LMmap = L.map(mapId, options);
 
-			this.addScalebar();
 			this.addLayersControl();
+			this.addScalebar();
     },
 
     addScalebar: function() {
@@ -65,12 +66,18 @@ var Traffic = Traffic || {};
 
     addLayersControl: function() {
     	var baseMaps = {
-			  "English": this.ENGLISH_TILE_LAYER,
+			  "English": this.ENGLISH_TILE_LAYER.addTo(this.LMmap),
 			  "International": this.LOCALIZED_TILE_LAYER
 			};
 
 			if(this.LMmap) {
-				L.control.layers(baseMaps).addTo(this.LMmap);
+				L.control.layers(baseMaps,{}).addTo(this.LMmap);
+
+				//This is to set basemap at the bottom so to let later added layers visible
+				this.LMmap.on('baselayerchange', function(evt){
+					var layer = evt.layer;
+					layer.bringToBack();
+				});
 			}
     }
 	};
@@ -937,7 +944,7 @@ var Traffic = Traffic || {};
 					url += '&w2=' + w2List.join(",");
 			}
 
-			A.app.segmentOverlay = L.tileLayer(url).addTo(A.app.map);
+			A.app.segmentOverlay = L.tileLayer(url).addTo(A.app.map).bringToFront();
 		},
 
 		updateTrafficTiles : function() {
@@ -1109,7 +1116,7 @@ var Traffic = Traffic || {};
 			if(A.app.map.hasLayer(A.app.segmentOverlay))
 				A.app.map.removeLayer(A.app.segmentOverlay);
 
-			A.app.dataOverlay = L.tileLayer('/tile/data?z={z}&x={x}&y={y}').addTo(A.app.map);
+			A.app.dataOverlay = L.tileLayer('/tile/data?z={z}&x={x}&y={y}').addTo(A.app.map).bringToFront();
 		},
 
 		clickRouting: function(evt) {
