4a04c89a566ab69d3e8be5989a51eacd62c0dbe2
dbenoff <dbenoff@gmail.com>
2016-06-09 15:48:10 -0700

    WBOT-139

--- src/main/resources/public/javascripts/views/exportdata-button.js
+++ src/main/resources/public/javascripts/views/exportdata-button.js
@@ -12,7 +12,24 @@ Traffic.views = Traffic.views || {};
     },
 
     clickExportData: function() {
+
         var params = A.app.sidebar.params;
+        if(!params.utcAdjustment)
+            params.utcAdjustment = A.app.instance.utcTimezoneOffset || 0;
+
+        if(!params.w1){
+            var w1List = A.app.sidebar.getWeek1List();
+            var w2List = A.app.sidebar.getWeek2List();
+
+            if(w1List && w1List.length > 0)
+                params.w1 = w1List.join(",");
+
+            if(this.$("#compare").prop( "checked" )) {
+                if (w2List && w2List.length > 0)
+                    params.w2 = w2List.join(",");
+            }
+        }
+
         $.ajax({
             type: "POST",
             url: "/csv",
